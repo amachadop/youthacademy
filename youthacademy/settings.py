4dc1b9c5b42407d844e9a4d81a0e8df270f79d1d
Alejandro <amacparedes@gamil.com>
2023-06-26 14:22:51 -0400

    second

--- youthacademy/settings.py
+++ youthacademy/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'youthacademy.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgress',
+        default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
 }
